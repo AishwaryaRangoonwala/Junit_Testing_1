78e9354ba0e6c4da59caacea28d3aa770de7587c
Aishwarya Rangoonwala <aishwaryarangoonwala@Aishwaryas-Laptop.local>
2025-12-03 14:38:41 +0530

    JUnit Assertions - Arrays, Iterable and Lines

--- src/test/java/com/aishwarya/DemoUtilsTest.java
+++ src/test/java/com/aishwarya/DemoUtilsTest.java
@@ -2,6 +2,8 @@ package com.aishwarya;
 
 import org.junit.jupiter.api.*;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 // @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -73,5 +75,33 @@ class DemoUtilsTest {
         int gradeTwo = 5;
         assertTrue(demoUtils.isGreater(gradeOne, gradeTwo), "This should return true");
         assertFalse(demoUtils.isGreater(gradeTwo, gradeOne), "This should return false");
+    }
+
+    @DisplayName("Array Equals")
+    @Test
+    void testArrayEquals() {
+        System.out.println("Running test: testArrayEquals");
+        String[] stringArray = {"A", "B", "C"};
+        assertArrayEquals(stringArray,
+                demoUtils.getFirstThreeLettersOfAlphabet(),
+                "Arrays should be the same");
+    }
+
+    @Test
+    @DisplayName("Iterable equals")
+    void testIterableEquals() {
+        System.out.println("Running test: testIterableEquals");
+        List<String> theList = List.of("luv", "2", "code");
+        assertIterableEquals(theList,
+                demoUtils.getAcademyInList(),
+                "Expected list should be same as actual list");
+    }
+
+    @Test
+    @DisplayName("Lines match")
+    void testLinesMatch() {
+        System.out.println("Running test: testLinesMatch");
+        List<String> theList = List.of("luv", "2", "code");
+        assertLinesMatch(theList, demoUtils.getAcademyInList(), "Lines should match");
     }
 }
